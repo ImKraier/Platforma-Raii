9049ee372ff5b243ec4f44c8293a095445ac4b8a
Kraier <kraiermarin11@gmail.com>
2022-02-21 19:57:50 +0000

    Finished redesign

--- Raii-redesign/assets/js/scripts.js
+++ Raii-redesign/assets/js/scripts.js
@@ -6,6 +6,46 @@ let category_name = $('#category-name');
 
 var active_element = $('.active');
 
+var semi_sidebar = $('.semi-sidebar');
+let semi_sidebar_bool = false;
+let content = $('.content');
+
+$(window).on('resize', function () {
+    if($(window).width() < 1500) {
+        semi_sidebar.addClass('collapsed')
+        semi_sidebar_bool = true;
+        $('.mobile-collapse > i').removeClass('fa-angle-double-left')
+        $('.mobile-collapse > i').addClass('fa-angle-double-right')
+    }
+});
+
+$(document).ready(function () {
+    if($(window).width() < 1500) {
+        semi_sidebar.addClass('collapsed')
+        semi_sidebar_bool = true;
+        $('.mobile-collapse > i').removeClass('fa-angle-double-left')
+        $('.mobile-collapse > i').addClass('fa-angle-double-right')
+    }
+});
+
+$('.mobile-collapse').click(function () {
+    if(semi_sidebar_bool == true) {
+        console.log('true')
+        semi_sidebar.removeClass('collapsed');
+        content.removeClass('collapsed');
+        semi_sidebar_bool = false;
+        $('.mobile-collapse > i').removeClass('fa-angle-double-right')
+        $('.mobile-collapse > i').addClass('fa-angle-double-left')
+    } else {
+        $('.mobile-collapse > i').removeClass('fa-angle-double-left')
+        $('.mobile-collapse > i').addClass('fa-angle-double-right')
+        console.log('false')
+        semi_sidebar.addClass('collapsed');
+        content.addClass('collapsed');
+        semi_sidebar_bool = true;
+    }
+});
+
 sidebar_li.each(function() {
     if(!$(this).hasClass('active')) {
         $($(this).data('target')).hide();
